c6208e7c64428afe93d58bb911db92f76bf45064
Claudia <tomus_clau@yahoo.com>
2024-10-17 08:52:20 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Destination from './pages/Destination/Destination';
+import Crew from './pages/Crew/Crew';
+import Technology from './pages/Technology/Technology';
+import Header from './components/Header/Header';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
